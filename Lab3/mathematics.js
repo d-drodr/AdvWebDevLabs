1c84516b5305c0938cb618d07b4b1d3cb010de8a
Derrick Rodriguez <dexter10679@gmail.com>
2023-06-22 20:24:44 -0500

    added main functions

---
+++ Lab3/mathematics.js
@@ -0,0 +1,20 @@
+/*
+simple functions to get user input and do operations
+
+calclulate function gets all data from input field and uses eval to take every literal
+
+clear function resets input field to blank string
+*/
+function addToInput(value){
+    document.getElementById('input').value += value;
+}
+
+function calculate(){
+    var input = document.getElementById('input').value;
+    var result = eval(input);
+    document.getElementById('result').innerHTML = "Result: " + result;
+}
+function clearAll(){
+    document.getElementById('input').value = '';
+    document.getElementById('result').innerHTML= '';
+}
