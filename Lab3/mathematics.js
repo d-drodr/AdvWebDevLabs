b29681c533d844f7eda2c41665658c77fc0c579b
Derrick Rodriguez <dexter10679@gmail.com>
2023-06-24 08:24:50 -0500

    complete working calc

--- Lab3/mathematics.js
+++ Lab3/mathematics.js
@@ -6,19 +6,43 @@ calclulate function gets all data from input field and uses eval to take every l
 clear function resets input field to blank string
 */
 function addToInput(value){
-    document.getElementById('input').value += value;
+    var input = document.getElementById('input');
+    var cursorPosition = input.selectionStart;
+    var inputValue = input.value;
+    
+    input.value = inputValue.slice(0, cursorPosition) + value + inputValue.slice(cursorPosition);
+    input.selectionStart = cursorPosition + value.length;
+    input.selectionEnd = cursorPosition + value.length;
+    
+    input.focus();
 }
 
 function calculate(){
     var input = document.getElementById('input').value;
-    var result = eval(input);
-    document.getElementById('result').innerHTML =  result;
+    input = input.replace(/sqrt()/g, 'Math.sqrt');
+    var result;
+    // Check if the input contains trigonometric functions
+  if (input.includes('sin') || input.includes('cos') || input.includes('tan')) {
+    try {
+      // Replace trigonometric function names with the js math funcions
+      input = input.replace(/sin()/g, 'Math.sin');
+      input = input.replace(/cos()/g, 'Math.cos');
+      input = input.replace(/tan()/g, 'Math.tan');
+      
+      // Evaluate the expression 
+      result = eval(input);
+    } catch (error) {
+      result = 'Error';
+    }
+  } else {
+    // Evaluate the expression directly 
+    result = eval(input);
+  }
+  
+  document.getElementById('result').innerHTML = result;
 }
+  
 function clearAll(){
     document.getElementById('input').value = '';
     document.getElementById('result').innerHTML= '';
-}
-function toggleScientific() {
-    var scientificButtons = document.querySelector('.scientific-buttons');
-    scientificButtons.classList.toggle('active');
 }
