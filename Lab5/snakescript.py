5fb66e3a63583076a8a14268a0edb795c6ae1bb9
Derrick Rodriguez <dexter10679@gmail.com>
2023-07-08 15:38:38 -0500

    reworked on script for simpler format

--- Lab5/snakescript.py
+++ Lab5/snakescript.py
@@ -1,9 +1,47 @@
 import requests
 import json
-
+import re
+#drodr
+#
 url = "https://michaelgathara.com/api/python-challenge"
+
+# on windows terminal, backlash is not accepted, escape each slash
+
+# https://stackoverflow.com/questions/54517547/curl-having-issue-with-brace-bracket
+
+# used following guide for api help
+# https://www.dataquest.io/blog/python-api-tutorial/
+
 
 response = requests.get(url)
 
-challenges = response.json()
-print(challenges)
+if response.status_code == 200:
+    challenges = response.json()
+
+    if challenges:
+        for problem in challenges:
+            problemID = problem.get('id')
+            problemText = problem.get('problem')
+
+            # divide operator values and problem values
+            
+            match = re.search(r'(\d+)\s*([+*/-])\s*(\d+)', problemText)
+            
+            if match:
+                operand1 = float(match.group(1))
+                operator = match.group(2)
+                operand2 = float(match.group(3))
+
+                # basic conditional to compute result based on operator type
+                if operator == '+':
+                    result = operand1 + operand2
+                elif operator == '-':
+                    result = operand1 - operand2
+                elif operator == '*':
+                    result = operand1 * operand2
+                elif operator == '/':
+                    result = operand1 / operand2
+
+                # output problemid and result
+                print(f"Problem {problemID}: {problemText} = {result}")
+            
